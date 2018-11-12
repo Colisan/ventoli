1723be0f7668da5cc86b50b5f513bb97e417f2f3
Colisan Loveteit <colisan@loveteit.fr>
2018-11-12 22:52:07 +0100

    Babylon.js test setup

--- http/test.php
+++ http/test.php
@@ -1,2 +1,16 @@
-<?php
-    echo "Hello world!";
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ventoli test</title>
+        <meta charset='utf-8'>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel='stylesheet' href='src/css/common.css'>
+        <link rel='stylesheet' href='src/css/game_render.css'>
+        <script src='src/js/common.js'></script>
+        <script src='src/js/game_render.js'></script>
+        <script src='src/lib/babylon/babylon.custom.js'></script>
+    </head>
+    <body>
+        <canvas id="renderCanvas"></canvas>
+    </body>
+</html>
