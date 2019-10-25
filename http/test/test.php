d8977750ccb175ad021537c8e975350a929b279b
Colisan <colisan@loveteit.fr>
2019-10-26 00:53:36 +0200

    init front vue

---
+++ http/test/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ventoli test</title>
+        <meta charset='utf-8'>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel='stylesheet' href='src/css/common.css'>
+        <link rel='stylesheet' href='src/css/game_render.css'>
+        <script src='src/js/common.js'></script>
+        <script src='src/js/game_render.js'></script>
+        <script src='src/lib/babylon/babylon.custom.js'></script>
+    </head>
+    <body>
+        <canvas id="renderCanvas"></canvas>
+    </body>
+</html>
