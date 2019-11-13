987199234c878fe048c73551adecadcc008c42e4
Colisan <colisan@loveteit.fr>
2019-11-13 15:41:33 +0100

    done with tests on controllers =)

--- ventoli-api/src/controller/AuthController.test.ts
+++ ventoli-api/src/controller/AuthController.test.ts
@@ -4,6 +4,7 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { mockReq, mockRes } from 'sinon-express-mock';
 import * as typeorm from 'typeorm';
+import JwtPayload from '../model/JwtPayload';
 import AuthController from './AuthController';
 import Player from '../entity/Player';
 
@@ -39,7 +40,7 @@ describe('AuthController login method', () => {
     expect(res.status).to.have.been.calledWith(400);
   });
 
-  it("sends http 401 credentials don't match", async () => {
+  it("sends http 401 playername don't exist in db", async () => {
     authController.playerRepository.findOneOrFail = sinon.stub().throws(new Error('Nobody hear you'));
 
     const req = mockReq({ body: { playername: 'Admin', password: '1234' } });
@@ -49,5 +50,46 @@ describe('AuthController login method', () => {
     expect(res.status).to.have.been.calledWith(401);
 
     authController.playerRepository.findOneOrFail = undefined;
+  });
+
+  it("sends http 401 password don't match", async () => {
+    const foundPlayer = new Player();
+    foundPlayer.id = 123;
+    foundPlayer.name = 'NotTheAdmin';
+    foundPlayer.password = 'th3Passw0rd';
+    foundPlayer.hashPassword();
+
+    authController.playerRepository.findOneOrFail = sinon.stub().returns(foundPlayer);
+
+    const req = mockReq({ body: { playername: foundPlayer.name, password: 'n0tTh3Passw0rd' } });
+    const res = mockRes();
+    await authController.login(req, res);
+
+    expect(res.status).to.have.been.calledWith(401);
+
+    authController.playerRepository.findOneOrFail = undefined;
+  });
+
+  it('sends token if auth successful', async () => {
+    const foundPlayer = new Player();
+    foundPlayer.id = 123;
+    foundPlayer.name = 'StillNot';
+    const clearPassword = 'zer0zer0';
+    foundPlayer.password = clearPassword;
+    foundPlayer.hashPassword();
+
+    authController.playerRepository.findOneOrFail = sinon.stub().returns(foundPlayer);
+    const fakeToken = 'signedT0ken';
+    const tokenStub = sinon.stub(JwtPayload.prototype, 'getSignedToken').returns(fakeToken);
+
+    const req = mockReq({ body: { playername: foundPlayer.name, password: clearPassword } });
+    const res = mockRes();
+    await authController.login(req, res);
+
+    expect(tokenStub).to.have.been.called;
+    expect(res.send).to.have.been.calledWith(fakeToken);
+
+    authController.playerRepository.findOneOrFail = undefined;
+    tokenStub.restore();
   });
 });
